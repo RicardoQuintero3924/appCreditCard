38529ca81e00af76aae4d9de90f63f1087223c40
RicardoQuintero3924 <rikrdoquinvilla3924@gmail.com>
2024-04-02 16:42:10 -0500

    cargando el proyecto al repo

---
+++ src/main/java/com/example/appcreditcard/AppCreditCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.appcreditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCreditCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCreditCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appcreditcard/AppCreditCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcreditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCreditCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
